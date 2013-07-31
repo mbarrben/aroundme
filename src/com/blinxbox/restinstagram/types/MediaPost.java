be9b9a97b528107ecabb2872f4131d01ae6f161e
mbarrben <mbarrben@gmail.com>
2013-07-31 16:50:20 +0200

    [ADD] location to MediaPost type

--- src/com/blinxbox/restinstagram/types/MediaPost.java
+++ src/com/blinxbox/restinstagram/types/MediaPost.java
@@ -39,362 +39,410 @@ import com.blinxbox.restinstagram.Instagram;
  */
 public class MediaPost implements Serializable {
 
-	/**
-	 * Represents an Instagram caption section.
-	 * 
-	 * @author Efi MK
-	 * 
-	 */
-	public static class Likes implements Serializable {
-		/**
-		 * Text of the comment.
-		 */
-		@Instagram
-		int count;
+    public static class Location implements Serializable {
+        @Instagram
+        double latitude;
 
-		/**
-		 * @return Number of likes.
-		 */
-		public int getCount() {
-			return count;
-		}
-	}
+        @Instagram
+        double longitude;
 
-	/**
-	 * Represents an Instagram caption section.
-	 * 
-	 * @author Efi MK
-	 * 
-	 */
-	public static class Caption implements Serializable {
-		/**
-		 * Text of the comment.
-		 */
-		@Instagram
-		String text;
+        @Instagram
+        String name;
 
-		/**
-		 * Caption id.
-		 */
-		@Instagram
-		String id;
+        @Instagram
+        String id;
 
-		/**
-		 * @return Text of the comment.
-		 */
-		public String getText() {
-			return text;
-		}
+        public String getName() {
+            return name;
+        }
 
-		/**
-		 * @return Caption id.
-		 */
-		public String getId() {
-			return id;
-		}
+        public String getId() {
+            return id;
+        }
 
-		public Caption() {
-		}
+        public double getLatitude() {
+            return latitude;
+        }
 
-		Caption(final String text, final String id) {
-			this.text = text;
-			this.id = id;
+        public double getLongitude() {
+            return longitude;
+        }
 
-		}
+        public Location() {
+        }
 
-	}
+        Location(final double latitude, final double longitude, final String name, final String id) {
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.name = name;
+            this.id = id;
+        }
 
-	/**
-	 * Represents an Instagram user.
-	 * 
-	 * @author Efi MK
-	 * 
-	 */
-	public static class User implements Serializable {
+    }
 
-		/**
-		 * For serialization.
-		 */
-		private static final long serialVersionUID = -9139733728844823420L;
+    /**
+     * Represents an Instagram caption section.
+     * 
+     * @author Efi MK
+     * 
+     */
+    public static class Likes implements Serializable {
+        /**
+         * Text of the comment.
+         */
+        @Instagram
+        int count;
 
-		/**
-		 * A link to the profile picture of the user.
-		 */
-		@Instagram
-		String profile_picture;
-		/**
-		 * User ID.
-		 */
-		@Instagram
-		String id;
-		/**
-		 * Full name of the user.
-		 */
-		@Instagram
-		String full_name;
+        /**
+         * @return Number of likes.
+         */
+        public int getCount() {
+            return count;
+        }
+    }
 
-		/**
-		 * User name.
-		 */
-		@Instagram
-		String username;
+    /**
+     * Represents an Instagram caption section.
+     * 
+     * @author Efi MK
+     * 
+     */
+    public static class Caption implements Serializable {
+        /**
+         * Text of the comment.
+         */
+        @Instagram
+        String text;
 
-		/**
-		 * @return A link to the profile picture of the user.
-		 */
-		public String getProfilePicture() {
-			return profile_picture;
-		}
+        /**
+         * Caption id.
+         */
+        @Instagram
+        String id;
 
-		/**
-		 * @return User ID.
-		 */
-		public String getId() {
-			return id;
-		}
+        /**
+         * @return Text of the comment.
+         */
+        public String getText() {
+            return text;
+        }
 
-		/**
-		 * @return Full name of the user.
-		 */
-		public String getFullName() {
-			return full_name;
-		}
+        /**
+         * @return Caption id.
+         */
+        public String getId() {
+            return id;
+        }
 
-		/**
-		 * @return User name.
-		 */
-		public String getUserName() {
-			return username;
-		}
-	}
+        public Caption() {
+        }
 
-	/**
-	 * Represents an image in Instagram. There are 3 types of images: <li>
-	 * low_resolution - with size of 306 * 306. <li>thumbnail - with size of 150
-	 * * 150. <li>standard_resolution - with size of 612 * 612.
-	 * 
-	 * Check the size by using {@link #getHeight()} and {@link #getWidth()}
-	 * 
-	 * @author Efi MK
-	 * 
-	 */
-	public static class Image implements Serializable {
-		/**
-		 * Used for serialization.
-		 */
-		private static final long serialVersionUID = 5811101144604271107L;
+        Caption(final String text, final String id) {
+            this.text = text;
+            this.id = id;
 
-		/**
-		 * Width of the image.
-		 */
-		@Instagram
-		private int width;
+        }
 
-		/**
-		 * Height of the image.
-		 */
-		@Instagram
-		private int height;
+    }
 
-		/**
-		 * From where the image can be retrieved.
-		 */
-		@Instagram
-		private String url;
+    /**
+     * Represents an Instagram user.
+     * 
+     * @author Efi MK
+     * 
+     */
+    public static class User implements Serializable {
 
-		/**
-		 * @return Width of the image.
-		 */
-		public int getWidth() {
-			return width;
-		}
+        /**
+         * For serialization.
+         */
+        private static final long serialVersionUID = -9139733728844823420L;
 
-		/**
-		 * @return Height of the image.
-		 */
-		public int getHeight() {
-			return height;
-		}
+        /**
+         * A link to the profile picture of the user.
+         */
+        @Instagram
+        String profile_picture;
+        /**
+         * User ID.
+         */
+        @Instagram
+        String id;
+        /**
+         * Full name of the user.
+         */
+        @Instagram
+        String full_name;
 
-		/**
-		 * @return From where the image can be retrieved.
-		 */
-		public String getUrl() {
-			return url;
-		}
+        /**
+         * User name.
+         */
+        @Instagram
+        String username;
 
-	}
+        /**
+         * @return A link to the profile picture of the user.
+         */
+        public String getProfilePicture() {
+            return profile_picture;
+        }
 
-	/**
-	 * Holds the images object. There are 3 types of images: <li>
-	 * low_resolution - with size of 306 * 306. <li>thumbnail - with size of 150
-	 * * 150. <li>standard_resolution - with size of 612 * 612.
-	 * 
-	 * @author Efi MK
-	 * 
-	 */
-	public static class Images implements Serializable {
-		/**
+        /**
+         * @return User ID.
+         */
+        public String getId() {
+            return id;
+        }
+
+        /**
+         * @return Full name of the user.
+         */
+        public String getFullName() {
+            return full_name;
+        }
+
+        /**
+         * @return User name.
+         */
+        public String getUserName() {
+            return username;
+        }
+    }
+
+    /**
+     * Represents an image in Instagram. There are 3 types of images: <li>
+     * low_resolution - with size of 306 * 306. <li>thumbnail - with size of 150
+     * * 150. <li>standard_resolution - with size of 612 * 612.
+     * 
+     * Check the size by using {@link #getHeight()} and {@link #getWidth()}
+     * 
+     * @author Efi MK
+     * 
+     */
+    public static class Image implements Serializable {
+        /**
+         * Used for serialization.
+         */
+        private static final long serialVersionUID = 5811101144604271107L;
+
+        /**
+         * Width of the image.
+         */
+        @Instagram
+        private int width;
+
+        /**
+         * Height of the image.
+         */
+        @Instagram
+        private int height;
+
+        /**
+         * From where the image can be retrieved.
+         */
+        @Instagram
+        private String url;
+
+        /**
+         * @return Width of the image.
+         */
+        public int getWidth() {
+            return width;
+        }
+
+        /**
+         * @return Height of the image.
+         */
+        public int getHeight() {
+            return height;
+        }
+
+        /**
+         * @return From where the image can be retrieved.
+         */
+        public String getUrl() {
+            return url;
+        }
+
+    }
+
+    /**
+     * Holds the images object. There are 3 types of images: <li>
+     * low_resolution - with size of 306 * 306. <li>thumbnail - with size of 150
+     * * 150. <li>standard_resolution - with size of 612 * 612.
+     * 
+     * @author Efi MK
+     * 
+     */
+    public static class Images implements Serializable {
+        /**
 		 * 
 		 */
-		private static final long serialVersionUID = 7940466671705419507L;
+        private static final long serialVersionUID = 7940466671705419507L;
 
-		/**
-		 * Low resolution image 306*305
-		 */
-		@Instagram
-		private Image low_resolution;
+        /**
+         * Low resolution image 306*305
+         */
+        @Instagram
+        private Image low_resolution;
 
-		/**
-		 * thumnail image. 150 *150
-		 */
-		@Instagram
-		private Image thumbnail;
+        /**
+         * thumnail image. 150 *150
+         */
+        @Instagram
+        private Image thumbnail;
 
-		/**
-		 * standard image. 612 * 612
-		 */
-		@Instagram
-		private Image standard_resolution;
+        /**
+         * standard image. 612 * 612
+         */
+        @Instagram
+        private Image standard_resolution;
 
-		/**
-		 * @return Low resolution image 306*305
-		 */
-		public Image getLowResolution() {
-			return low_resolution;
-		}
+        /**
+         * @return Low resolution image 306*305
+         */
+        public Image getLowResolution() {
+            return low_resolution;
+        }
 
-		/**
-		 * @return thumnail image. 150 *150
-		 */
-		public Image getThumbnail() {
-			return thumbnail;
-		}
+        /**
+         * @return thumnail image. 150 *150
+         */
+        public Image getThumbnail() {
+            return thumbnail;
+        }
 
-		/**
-		 * @return standard image. 612 * 612
-		 */
-		public Image getStandardResolution() {
-			return standard_resolution;
-		}
+        /**
+         * @return standard image. 612 * 612
+         */
+        public Image getStandardResolution() {
+            return standard_resolution;
+        }
 
-	}
+    }
 
-	/**
-	 * Each post has one image with 3 resolutions. This variable holds pointers
-	 * to the images.
-	 */
-	@Instagram
-	private Images images;
+    @Instagram
+    private Location location;
 
-	/**
-	 * Time since epoch this post was created.
-	 */
-	@Instagram
-	private long created_time;
-	/**
-	 * User who posted this post.
-	 */
-	@Instagram
-	private User user;
+    /**
+     * Each post has one image with 3 resolutions. This variable holds pointers
+     * to the images.
+     */
+    @Instagram
+    private Images images;
 
-	/**
-	 * Link to the post.
-	 */
-	@Instagram
-	private String link;
+    /**
+     * Time since epoch this post was created.
+     */
+    @Instagram
+    private long created_time;
+    /**
+     * User who posted this post.
+     */
+    @Instagram
+    private User user;
 
-	/**
-	 * Caption for the post.
-	 */
-	@Instagram
-	private Caption caption;
+    /**
+     * Link to the post.
+     */
+    @Instagram
+    private String link;
 
-	/**
-	 * Likes for the current post.
-	 */
-	@Instagram
-	private Likes likes;
+    /**
+     * Caption for the post.
+     */
+    @Instagram
+    private Caption caption;
 
-	/**
-	 * Post ID.
-	 */
-	@Instagram
-	private String id;
+    /**
+     * Likes for the current post.
+     */
+    @Instagram
+    private Likes likes;
 
-	/**
-	 * Holds post tags.
-	 */
-	@Instagram
-	private List<String> tags;
+    /**
+     * Post ID.
+     */
+    @Instagram
+    private String id;
 
-	/**
-	 * Used for serialization.
-	 */
-	private static final long serialVersionUID = 2L;
+    /**
+     * Holds post tags.
+     */
+    @Instagram
+    private List<String> tags;
 
-	/**
-	 * @return Each post has one image with 3 resolutions. Returns these 3
-	 *         resolutions.
-	 */
-	public Images getImages() {
-		return images;
-	}
+    /**
+     * Used for serialization.
+     */
+    private static final long serialVersionUID = 2L;
 
-	/**
-	 * @return Time since epoch this post was created. In seconds.
-	 */
-	public Date getCreatedTime() {
-		return new Date(created_time);
-	}
+    public Location getLocation() {
+        return location;
+    }
 
-	/**
-	 * @return User who posted this post.
-	 */
-	public User getUser() {
-		return user;
-	}
+    /**
+     * @return Each post has one image with 3 resolutions. Returns these 3
+     *         resolutions.
+     */
+    public Images getImages() {
+        return images;
+    }
 
-	/**
-	 * @return Caption for the post. In case the post does not have a caption
-	 *         return an empty (not null).
-	 */
-	public Caption getCaption() {
-		Caption returnValue;
-		if (caption == null) {
-			returnValue = new Caption("", "99999");
-		} else {
-			returnValue = caption;
-		}
-		return returnValue;
-	}
+    /**
+     * @return Time since epoch this post was created. In seconds.
+     */
+    public Date getCreatedTime() {
+        return new Date(created_time);
+    }
 
-	/**
-	 * @return Link to the post.
-	 */
-	public String getLink() {
-		return link;
-	}
+    /**
+     * @return User who posted this post.
+     */
+    public User getUser() {
+        return user;
+    }
 
-	/**
-	 * @return Post ID.
-	 */
-	public String getId() {
-		return id;
-	}
+    /**
+     * @return Caption for the post. In case the post does not have a caption
+     *         return an empty (not null).
+     */
+    public Caption getCaption() {
+        Caption returnValue;
+        if (caption == null) {
+            returnValue = new Caption("", "99999");
+        } else {
+            returnValue = caption;
+        }
+        return returnValue;
+    }
 
-	/**
-	 * @return Likes for the current post.
-	 */
-	public Likes getLikes() {
-		return likes;
-	}
+    /**
+     * @return Link to the post.
+     */
+    public String getLink() {
+        return link;
+    }
 
-	/**
-	 * @return Holds post tags.
-	 */
-	public List<String> getTags() {
-		return tags;
-	}
+    /**
+     * @return Post ID.
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @return Likes for the current post.
+     */
+    public Likes getLikes() {
+        return likes;
+    }
+
+    /**
+     * @return Holds post tags.
+     */
+    public List<String> getTags() {
+        return tags;
+    }
 
 }
